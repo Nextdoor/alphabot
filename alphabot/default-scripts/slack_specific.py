3128b0f19ae8a9e2aaed9ec674660e9bca5ba209
Mikhail Simin <mikhail@nextdoor.com>
2016-11-23 14:49:52 -0800

    Fix slack scripts

--- alphabot/default-scripts/slack_specific.py
+++ alphabot/default-scripts/slack_specific.py
@@ -10,14 +10,6 @@ bot = alphabot.bot.get_instance()
 log = logging.getLogger(__name__)
 
 
-#@bot.on_schedule(minute='*')
-#@gen.coroutine
-#def check_connection():
-#    # TODO: figure out how to check connection and reconnect on failure.
-#    # yield bot.check_connection()
-#    pass
-
-
 @bot.on(ok=False, error={"code": -1,
                          "msg": "slow down, too many messages..."})
 @gen.coroutine
